2928266d6d6d44c822f63b7153cd189bd17841d2
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-02-06 15:46:27 +1000

    Cleaned up Dynamic Film text a bit

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -314,9 +314,9 @@ return {
             j_dvrprv_dynamic_film = {
                 name = "Dynamic Film",
                 text = {
-                    "This Joker gains {C:chips}+#2#{} Chips",
-                    "every time an {C:cine}Exchange Coupon",
-                    "progresses",
+                    "This Joker gains",
+                    "{C:chips}+#2#{} Chips every time an",
+                    "{C:cine}Exchange Coupon{} progresses",
                     "{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips)"
                 }
             }
